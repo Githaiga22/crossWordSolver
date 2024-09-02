f57bcd3ec3e6504940a22c95403cd08cf9bc4118
jowala <owalajoab@gmail.com>
2024-09-02 10:40:06 +0300

    chore: check double words

---
+++ crosswordSolver.js
@@ -0,0 +1,21 @@
+function crosswordSolver(emptyPuzzle,words){
+
+
+}
+
+function checkDouble(words){
+    let j=1
+    for (let i=0; i<words.length-1;){
+        for (;j<words.length;){
+            if (words[i] === words[j]){
+                return "Error"
+            }
+            j++
+        }
+        j = i+2
+        i++
+    }
+    return "Success"
+}
+// let words = ['casa', 'alan', 'ciao', 'anta','casa']
+// console.log(checkDouble(words))
